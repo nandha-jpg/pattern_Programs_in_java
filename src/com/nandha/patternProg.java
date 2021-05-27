b0b5020def63533f4a7d0362cbd04b5f82735848
Nandha Kumar <48424140+nandha-jpg@users.noreply.github.com>
2021-05-27 16:47:43 +0530

    added comments to display output in different forms

--- src/com/nandha/patternProg.java
+++ src/com/nandha/patternProg.java
@@ -6,7 +6,14 @@ public class patternProg {
         //int column=3;
         for(int row=1; row<=3;row++ ){
             for (int column=1; column <= row; column++){
-                System.out.print(column);
+                System.out.print(row); //output as below:
+                                        //1
+                                        //22
+                                        //333
+                //System.out.println(column); output as below
+                //1
+                //12
+                //123
             }
             System.out.println();
 
