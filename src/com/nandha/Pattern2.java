e220bf4280b5f094fe5e51f569ce5d95eeef1c87
Nandha Kumar <48424140+nandha-jpg@users.noreply.github.com>
2021-05-27 17:37:01 +0530

    added comments

--- src/com/nandha/Pattern2.java
+++ src/com/nandha/Pattern2.java
@@ -5,6 +5,13 @@ public class Pattern2 {
         for(int row=5; row>=1;row--){
             for(int column=5; column>=row;column--){
                 System.out.print(row);
+                //System.out.print(column);
+                //output:
+                //5
+                //54
+                //543
+                //5432
+                //54321
             }
 
             System.out.println();
